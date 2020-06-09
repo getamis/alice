d313db90d92b3ec691b3e1c9279475cea0eb13fc
mark.lin <mark@maicoin.com>
2020-06-09 16:55:17 +0800

    linter: enable go sec

--- example/config/config.go
+++ example/config/config.go
@@ -34,5 +34,5 @@ func WriteYamlFile(yamlData interface{}, filePath string) error {
 	if err != nil {
 		return err
 	}
-	return ioutil.WriteFile(filePath, data, 0644)
+	return ioutil.WriteFile(filePath, data, 0600)
 }
