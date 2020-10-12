e3dc681e7ee8fb97ebe987e8714e4f1d9fa7160c
Alan Chen <alan@maicoin.com>
2020-10-12 14:51:47 +0800

    crypto/oprf: implement a fixed hash function: hashUserData

--- crypto/oprf/oprf_test.go
+++ crypto/oprf/oprf_test.go
@@ -53,6 +53,21 @@ var _ = Describe("oprf test", func() {
 		Entry("092834729-37492374", []byte("092834729-37492374"), big.NewInt(999999)),
 	)
 
+	// Warn: CAN NOT CHANGE THESE TESTS
+	DescribeTable("Fix Vector()", func(pw []byte, k *big.Int, expectedString string) {
+		got, err := ComputeShare(k, pw, secp256k1Hasher)
+		Expect(err).Should(BeNil())
+		expected, _ := new(big.Int).SetString(expectedString, 10)
+		Expect(err).Should(BeNil())
+		Expect(got.Cmp(expected) == 0).Should(BeTrue())
+	},
+		Entry("random k", []byte("cy hahahahahaha"), big.NewInt(2), "39762705969790288698147785435158915025771216702954110676076686122230821070412"),
+		Entry("random k", []byte("cy hahahahahaha"), big.NewInt(3), "66160902141901830464806996240648401353488586023602583499574451231396974735128"),
+		Entry("asdfsasddfgdfs", []byte("asdfsasddfgdfs"), big.NewInt(32000000), "75842743101060490522393730082400262068787578753446434023016710045513511388077"),
+		Entry("092834729837492374", []byte("092834729837492374"), big.NewInt(10000), "29704828473288093072032800362891643369290824425550073036816806344369222682398"),
+		Entry("092834729-37492374", []byte("092834729-37492374"), big.NewInt(999999), "80863401229445569258591851410919083324501633198301704812368958895625043366063"),
+	)
+
 	Context("Negative case", func() {
 		It("ErrNotInRange", func() {
 			responser, err := NewResponserWithK(big.NewInt(0))
