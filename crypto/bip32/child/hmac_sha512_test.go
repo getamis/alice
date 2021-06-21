30e7327ac368b6816e629becc9a9e78144f38a08
ChihYunChuang <chihyun@maicoin.com>
2021-06-21 15:43:25 +0800

    crypto/bip32/child: add hmac tests

--- crypto/bip32/child/hmac_sha512_test.go
+++ crypto/bip32/child/hmac_sha512_test.go
@@ -20,6 +20,7 @@ import (
 	"crypto/sha512"
 	"encoding/binary"
 
+	"github.com/getamis/alice/crypto/utils"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
@@ -61,6 +62,37 @@ var _ = Describe("Bip32 test", func() {
 		Entry("input:", []byte("yaya"), []byte("MarkIsGod")),
 		Entry("input:", []byte("yaya"), []byte("IanIsGood")),
 	)
+
+	It("NewHmacSha512()", func() {
+		key, err := utils.GenRandomBytes(517)
+		Expect(err).Should(BeNil())
+		hmac512 := NewHmacSha512(key)
+		Expect(hmac512).ShouldNot(BeNil())
+	})
+
+	It("Reset()", func() {
+		hmac512 := NewHmacSha512([]byte{1})
+		hmac512.Reset()
+	})
+
+	It("ComputeOutputFirstBlockHash()", func() {
+		hmac512 := NewHmacSha512([]byte{1})
+		_, err := hmac512.ComputeOutputFirstBlockHash()
+		Expect(err).Should(BeNil())
+	})
+
+	It("GetOutputSecondBlockHash()", func() {
+		hmac512 := NewHmacSha512([]byte{1})
+		_, err := hmac512.GetOutputSecondBlockHash([]byte{2})
+		Expect(err).Should(BeNil())
+	})
+
+	It("getSecondBlockHash()", func() {
+		message, err := utils.GenRandomBytes(300)
+		Expect(err).Should(BeNil())
+		_, err = getSecondBlockHash(message, []byte{1})
+		Expect(err).ShouldNot(BeNil())
+	})
 })
 
 func uint64SliceToByteSlice(input []uint64) []byte {
