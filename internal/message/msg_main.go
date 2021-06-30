cb6a10cdff213ed2e57e16ea10e7487a56280b2a
mark <mark@amis.com>
2021-06-30 20:42:41 +0800

    internal/message: handle panic error

--- internal/message/msg_main.go
+++ internal/message/msg_main.go
@@ -17,6 +17,7 @@ package message
 import (
 	"context"
 	"errors"
+	"fmt"
 	"sync"
 
 	"github.com/getamis/alice/internal/message/types"
@@ -94,7 +95,9 @@ func (t *MsgMain) GetFinalError() error {
 
 func (t *MsgMain) messageLoop(ctx context.Context) (err error) {
 	defer func() {
-		if err == nil {
+		if pErr := recover(); pErr != nil {
+			_ = t.setState(types.StateFailed, fmt.Errorf("panic: %v", pErr))
+		} else if err == nil {
 			_ = t.setState(types.StateDone, nil)
 		} else {
 			_ = t.setState(types.StateFailed, err)
