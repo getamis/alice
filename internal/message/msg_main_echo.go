132375eab9c006fa937cc62e8a7d2c0656ecd29d
mark <mark@amis.com>
2022-05-31 12:21:16 +0800

    internal/message: implement echo protocol

---
+++ internal/message/msg_main_echo.go
@@ -0,0 +1,97 @@
+// Copyright © 2020 AMIS Technologies
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package message
+
+import (
+	"bytes"
+	"errors"
+	"sync"
+
+	"github.com/getamis/alice/internal/message/types"
+	"github.com/getamis/sirius/log"
+)
+
+var (
+	ErrDifferentHash = errors.New("different hash")
+)
+
+type EchoMsgMain struct {
+	logger log.Logger
+	pm     types.PeerManager
+	mu     sync.Mutex
+	// keep msgs
+	echoMsgs map[types.MessageType]map[string]*echoMessage
+	next     types.MessageMain
+}
+
+type echoMessage struct {
+	hash  []byte
+	count uint32
+}
+
+func NewEchoMsgMain(next types.MessageMain, pm types.PeerManager, ts ...types.MessageType) types.MessageMain {
+	msgs := make(map[types.MessageType]map[string]*echoMessage)
+	for _, t := range ts {
+		msgs[t] = make(map[string]*echoMessage)
+	}
+	return &EchoMsgMain{
+		logger:   log.New("service", "EchoMsgMain"),
+		pm:       pm,
+		echoMsgs: msgs,
+		next:     next,
+	}
+}
+
+// NOTE: Avoid duplicate messages from the same peer should be handled in the caller
+func (t *EchoMsgMain) AddMessage(msg types.Message) error {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+
+	// Check if not echo messages
+	echoMsg, ok := t.echoMsgs[msg.GetMessageType()]
+	if !ok {
+		return t.next.AddMessage(msg)
+	}
+
+	hash, err := msg.Hash()
+	if err != nil {
+		return err
+	}
+	mId := msg.GetId()
+	m, ok := echoMsg[mId]
+	if !ok {
+		// Broadcast to other peers
+		for _, id := range t.pm.PeerIDs() {
+			if mId != id {
+				t.pm.MustSend(id, msg)
+			}
+		}
+		echoMsg[mId] = &echoMessage{
+			hash: hash,
+		}
+		m = echoMsg[mId]
+	} else if !bytes.Equal(m.hash, hash) {
+		return ErrDifferentHash
+	}
+	m.count++
+
+	// Not handle if the message count is not enough
+	if m.count < t.pm.NumPeers() {
+		return nil
+	}
+	// Clear count
+	m.count = 0
+	return t.next.AddMessage(msg)
+}
---
+++ internal/message/msg_main_echo_test.go
@@ -0,0 +1,98 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package message
+
+import (
+	"github.com/getamis/alice/internal/message/types"
+	"github.com/getamis/alice/internal/message/types/mocks"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("EchoMsgMain", func() {
+	var (
+		msgMain         types.MessageMain
+		mockMessageMain *mocks.MessageMain
+		mockPeerManager *mocks.PeerManager
+		mockMsg         *mocks.Message
+
+		echoMsgType    = types.MessageType(10)
+		nonEchoMsgType = types.MessageType(11)
+		hash           = []byte("hash")
+	)
+	BeforeEach(func() {
+		mockMsg = new(mocks.Message)
+		mockMessageMain = new(mocks.MessageMain)
+		mockPeerManager = new(mocks.PeerManager)
+		msgMain = NewEchoMsgMain(mockMessageMain, mockPeerManager, echoMsgType)
+	})
+
+	AfterEach(func() {
+		mockMessageMain.AssertExpectations(GinkgoT())
+		mockPeerManager.AssertExpectations(GinkgoT())
+		mockMsg.AssertExpectations(GinkgoT())
+	})
+
+	Context("AddMessage", func() {
+		It("should be ok for not echo message", func() {
+			mockMsg.On("GetMessageType").Return(nonEchoMsgType).Once()
+			mockMessageMain.On("AddMessage", mockMsg).Return(nil).Once()
+			err := msgMain.AddMessage(mockMsg)
+			Expect(err).Should(BeNil())
+		})
+
+		Context("echo messages", func() {
+			msgId := "id"
+			otherPeerId := "other-id"
+			It("should be ok for the first message", func() {
+				mockMsg.On("GetMessageType").Return(echoMsgType).Once()
+				mockMsg.On("Hash").Return(hash, nil).Once()
+				mockMsg.On("GetId").Return(msgId).Once()
+				mockPeerManager.On("PeerIDs").Return([]string{msgId, otherPeerId}).Once()
+				mockPeerManager.On("MustSend", otherPeerId, mockMsg).Once()
+				mockPeerManager.On("NumPeers").Return(uint32(1)).Once()
+				mockMessageMain.On("AddMessage", mockMsg).Return(nil).Once()
+				err := msgMain.AddMessage(mockMsg)
+				Expect(err).Should(BeNil())
+			})
+
+			It("should be ok for the first message but not handle", func() {
+				mockMsg.On("GetMessageType").Return(echoMsgType).Once()
+				mockMsg.On("Hash").Return(hash, nil).Once()
+				mockMsg.On("GetId").Return(msgId).Once()
+				mockPeerManager.On("PeerIDs").Return([]string{msgId, otherPeerId}).Once()
+				mockPeerManager.On("MustSend", otherPeerId, mockMsg).Once()
+				mockPeerManager.On("NumPeers").Return(uint32(2)).Once()
+				err := msgMain.AddMessage(mockMsg)
+				Expect(err).Should(BeNil())
+			})
+
+			It("different hash", func() {
+				mockMsg.On("GetMessageType").Return(echoMsgType).Once()
+				mockMsg.On("Hash").Return(hash, nil).Once()
+				mockMsg.On("GetId").Return(msgId).Once()
+				mockPeerManager.On("PeerIDs").Return([]string{msgId, otherPeerId}).Once()
+				mockPeerManager.On("MustSend", otherPeerId, mockMsg).Once()
+				mockPeerManager.On("NumPeers").Return(uint32(2)).Once()
+				err := msgMain.AddMessage(mockMsg)
+				Expect(err).Should(BeNil())
+
+				mockMsg.On("GetMessageType").Return(echoMsgType).Once()
+				mockMsg.On("Hash").Return([]byte("wrong hash"), nil).Once()
+				mockMsg.On("GetId").Return(msgId).Once()
+				err = msgMain.AddMessage(mockMsg)
+				Expect(err).Should(Equal(ErrDifferentHash))
+			})
+		})
+	})
+})
