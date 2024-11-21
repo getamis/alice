ddfdfc48d45a9ec0531c45dd46b012235de7ab2f
ChihYunChuang <chihyun@maicoin.com>
2024-11-21 15:24:21 +0800

    add curve type, slip10 type, and pubKey compression

--- crypto/elliptic/ed25519_test.go
+++ crypto/elliptic/ed25519_test.go
@@ -14,9 +14,11 @@
 package elliptic
 
 import (
+	"encoding/hex"
 	"math/big"
 
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 )
 
@@ -31,4 +33,13 @@ var _ = Describe("ed25519", func() {
 			Expect(negY.Cmp(scalY) == 0).Should(BeTrue())
 		})
 	})
+	// Test vectors : https://asecuritysite.com/ecc/eddsa4
+	DescribeTable("Compressed PubKey", func(secrethex string, expected string) {
+		secret, _ := new(big.Int).SetString(secrethex, 16)
+		Expect(hex.EncodeToString(Ed25519().CompressedPublicKey(secret, "test")) == expected).Should(BeTrue())
+	},
+		Entry("case1:", "9d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60", "d75a980182b10ab7d54bfed3c964073a0ee172f3daa62325af021a68f707511a"),
+		Entry("case2:", "4ccd089b28ff96da9db6c346ec114e0f5b8a319f35aba624da8cf6ed4fb8a6fb", "3d4017c3e843895a92b70aa74d1b7ebc9c982ccf2ec4968cc0cd55f12af4660c"),
+		Entry("case3:", "c5aa8df43f9f837bedb7442f31dcb7b166d38535076f094b85ce3a2e0b4458f7", "fc51cd8e6218a1a38da47ed00230f0580816ed13ba3303ac5deb911548908025"),
+	)
 })
--- crypto/elliptic/elliptic_curve.go
+++ crypto/elliptic/elliptic_curve.go
@@ -28,3 +28,49 @@ func (c *ellipticCurve) Neg(x, y *big.Int) (*big.Int, *big.Int) {
 	NegY := new(big.Int).Neg(y)
 	return new(big.Int).Set(x), NegY.Mod(NegY, c.Curve.Params().P)
 }
+
+func (c *ellipticCurve) Type() string {
+	if c.Params().N.Cmp(p256Curve.Params().N) == 0 {
+		return "P256"
+	}
+	if c.Params().N.Cmp(secp256k1Curve.Params().N) == 0 {
+		return "secp256k1"
+	}
+	return "None"
+}
+
+func (c *ellipticCurve) Slip10SeedList() []byte {
+	if c.Params().N.Cmp(p256Curve.Params().N) == 0 {
+		return []byte("Bitcoin seed")
+	}
+	if c.Params().N.Cmp(secp256k1Curve.Params().N) == 0 {
+		return []byte("Bitcoin seed")
+	}
+	return []byte("None")
+}
+
+// WARN: Only support P256 and Secp256k1
+func (c *ellipticCurve) CompressedPublicKey(secret *big.Int, method string) []byte {
+	/* Returns the compressed bytes for this point.
+	   If pt.y is odd, 0x03 is pre-pended to pt.x.
+	   If pt.y is even, 0x02 is pre-pended to pt.x.
+	   Returns:
+	       bytes: Compressed byte representation.
+	*/
+	x, y := c.ScalarBaseMult(secret.Bytes())
+	xBytePadding := x.Bytes()
+	if len(x.Bytes()) < 32 {
+		padding := make([]byte, 32-len(x.Bytes()))
+		xBytePadding = append(padding, xBytePadding...)
+	}
+	if new(big.Int).And(y, big1).Cmp(big1) == 0 {
+		padding := make([]byte, 1)
+		padding[0] = 3
+		xBytePadding = append(padding, xBytePadding...)
+	} else {
+		padding := make([]byte, 1)
+		padding[0] = 2
+		xBytePadding = append(padding, xBytePadding...)
+	}
+	return xBytePadding
+}
